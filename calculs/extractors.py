beeab26eceb1b23ac09c4ed1b617433ce8ed5bb9
rivka-yuliia <juliacreadora@gmail.com>
2023-11-23 18:26:42 +0200

    alternative rate extractor

--- calculs/extractors.py
+++ calculs/extractors.py
@@ -3,7 +3,10 @@ Extract currency exchange rate.
 """
 
 import os
+from bs4 import BeautifulSoup
 import requests
+
+from calculs.currencies import currencies
 
 from dotenv import load_dotenv
 
@@ -45,3 +48,42 @@ class RateExtractor:
         """Convert the rate to rounded float number."""
 
         return round(float(data), 2)
+
+
+class RateScrapeExtractor:
+    """Extract exchange rate by scraping."""
+
+    base_url = 'https://www.x-rates.com/calculator/'
+    currencies = currencies
+
+    def get_rate(self, in_currency: str, out_currency: str) -> float:
+        """Scrape and return exchange rate of a currency pair."""
+
+        params = {
+            'from': in_currency,
+            'to': out_currency,
+            'amount': 1
+        }
+
+        content = requests.get(self.base_url, params=params).text
+        soup = BeautifulSoup(content, 'html.parser')
+        rate = soup.find('span', class_='ccOutputRslt').get_text()
+
+        return self.clean_data(rate)
+
+    def get_currencies(self):
+        """Return list of available currencies."""
+
+        return self.currencies
+
+    @staticmethod
+    def clean_data(data: str):
+        """Convert the rate to rounded float number."""
+
+        return round(float(data[:-4]), 2)
+
+
+# if __name__ == '__main__':
+#     exr = RateScrapeExtractor()
+#     print(exr.get_currencies().keys())
+    # print(exr.get_rate('USD', 'ILS'))
